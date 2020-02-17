6f90846e6da90aca870124082c5e4f25e2ef5320
Alice <alice.easey@gmail.com>
2020-02-17 08:12:13 +0000

    Styled background and buttons, fixed alignment issues with grid

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-const keys = document.querySelectorAll('[data-key');
+const keys = document.querySelectorAll('[data-key]');
+const operators = document.querySelectorAll('.operator');
+const point = document.querySelector('[data-key="."]');
 const display = document.querySelector('.answer');
 keys.forEach(key => key.addEventListener('click', getInput));
 
+const maxWidth = 20;
+let width = 0;
 let displayValue = "";
 let rVal = "";
 let lVal = "";
 let operator = "";
 let isOperatedOn = false;
+let impossible = false;
 
-function getInput(e) {
+function getInput() {
     let key = this.dataset.key;
 
     if(key === 'C') {
@@ -16,25 +21,51 @@ function getInput(e) {
         return;
     }
 
+    if(impossible) return;
+
     if(key === '=') {
-        calcAnswer();
+        displayValue = calcAnswer();
+        isOperatedOn = false;
+        operator = "";
+        
+    } else if(width === maxWidth) {
         return;
-    }
-
-    if(this.classList.contains('operator')) {
-        isOperatedOn = true;
-        operator = key;
-    }
-
-    if(isOperatedOn && this.classList.contains('number')) {
-        rVal += key;
-    }
+    } 
     else if(this.classList.contains('number')) {
-        lVal += key;
+        // Work out if it's adding to the left side or right side
+        if(operatorSelected()) {
+            displayValue = "";
+            console.log("OPERATOR SELECTED");
+        }
+
+        if(isOperatedOn) {
+            // right side
+            rVal += key;
+        } else {
+            lVal += key;
+        }
+        displayValue += key;
+
+        if(key === '.') {
+            point.disabled = true;
+        }
+
+        toggleSelected(key);
+        
+    }
+    else if(this.classList.contains('operator')) {
+        // Work out whether we need to do a calculation or not
+        
+        if(rVal)
+            displayValue = calcAnswer();
+        operator = key;
+        isOperatedOn = true;
+        point.disabled = false;
+        toggleSelected(key);
     }
 
-    displayValue += this.dataset.key;
     display.textContent = displayValue;
+    width = display.innerHTML.length;
 }
 
 function clearDisplay() {
@@ -42,18 +73,45 @@ function clearDisplay() {
     rVal = "";
     lVal = "";
     operator = "";
-    display.textContent = displayValue;
+    display.textContent = "";
     isOperatedOn = false;
+    width = 0;
+    impossible = false;
+    point.disabled = false;
 }
 
 function calcAnswer() {
-    isOperatedOn = false;
-    let answer = operate(operator, parseInt(lVal), parseInt(rVal));
+    let answer = operate(operator, parseFloat(lVal), parseFloat(rVal));
+
+    if(impossible) return answer;
+    
     lVal = answer;
     rVal = "";
-    operator = "";
-    displayValue = answer;
-    display.textContent = answer;
+
+    // Don't let the answer length overflow
+    let answerLength = answer;
+    answerLength = Math.floor(answerLength).toString().length + 1;
+    return parseFloat(answer.toFixed(maxWidth - answerLength));
+}
+
+function toggleSelected(key) {
+    operators.forEach(operator => {
+        if(operator.dataset.key === key) {
+            operator.classList.add('selected');
+        } else {
+            operator.classList.remove('selected');
+        }
+    });
+}
+
+function operatorSelected() {
+    let found = false;
+    operators.forEach(operator => {
+        if(operator.classList.contains('selected')) {
+            found = true;
+        }
+    });
+    return found;
 }
 
 /* ****************************
@@ -73,7 +131,14 @@ function operate(op, a, b) {
         case '*':
             return multiply(a, b);
         case '/':
-            return divide(a, b);
+        {
+            if(b === 0) {
+                impossible = true;
+                return 'Dividing by zero is impossible';
+            }
+            else
+                return divide(a, b);
+        }
         default:
             return 'Operator not recognized';
     }
